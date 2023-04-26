737c9cb36878b0ee90567c3c8fa533f985393dd1
Jeonkukil <a0211a@naver.com>
2023-04-26 17:25:25 +0900

    기타

---
+++ lib/view/pages/loding/lodingpage.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Lodingpage extends StatefulWidget {
+  const Lodingpage({Key? key}) : super(key: key);
+
+  @override
+  State<Lodingpage> createState() => _LodingpageState();
+}
+
+class _LodingpageState extends State<Lodingpage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
--- lib/view/pages/main/main_page/components/main_event_categories.dart
+++ lib/view/pages/main/main_page/components/main_event_categories.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:sporting_app/view/components/my_button.dart';
+import 'package:sporting_app/view/pages/dummy/honorary/honorary.dart';
+import 'package:sporting_app/view/pages/dummy/player/player.dart';
+import 'package:sporting_app/view/pages/dummy/recruitment/recruitment.dart';
+import 'package:sporting_app/view/pages/dummy/stadium/stadium.dart';
 
 class MainEventCategories extends StatelessWidget {
   const MainEventCategories({Key? key}) : super(key: key);
@@ -8,10 +12,10 @@ class MainEventCategories extends StatelessWidget {
   Widget build(BuildContext context) {
     return SliverGrid(
       delegate: SliverChildListDelegate([
-        _buildSportIcon("assets/images/oauth/google.png", "몰루?"),
-        _buildSportIcon("assets/images/oauth/google.png", "몰루?"),
-        _buildSportIcon("assets/images/oauth/google.png", "몰루?"),
-        _buildSportIcon("assets/images/oauth/google.png", "몰루?"),
+        _buildIcon(context, "assets/images/icons/tr.png", "명예의전당"),
+        _buildIcon2(context, "assets/images/icons/st.png", "이달의 구장"),
+        _buildIcon3(context, "assets/images/logo/sporting.png", "회사소개"),
+        _buildIcon4(context, "assets/images/icons/ut.png", "슈퍼플레이"),
       ]),
       gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
         crossAxisCount: 4,
@@ -21,12 +25,14 @@ class MainEventCategories extends StatelessWidget {
     );
   }
 
-  Column _buildSportIcon(String image, String sportName) {
+  Column _buildIcon(BuildContext context,String image, String sportName) {
     return Column(
         mainAxisSize: MainAxisSize.min,
         children: [
           MyButton(
-            funButton: () {},
+            funButton: () {
+              Navigator.of(context).push(MaterialPageRoute(builder: (_)=> Honorarypage()));
+            },
             image: image,
           ),
           Text(
@@ -35,5 +41,57 @@ class MainEventCategories extends StatelessWidget {
           ),
         ],
       );
+  }
+  Column _buildIcon2(BuildContext context, String image, String sportName) {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        MyButton(
+          funButton: () {
+            Navigator.of(context).push(MaterialPageRoute(builder: (_)=> Recruitment()));
+          },
+          image: image,
+        ),
+        Text(
+          sportName,
+          style: const TextStyle(fontSize: 10, fontWeight: FontWeight.bold),
+        ),
+      ],
+    );
+  }
+  Column _buildIcon3(BuildContext context, String image, String sportName) {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        MyButton(
+          funButton: () {
+            Navigator.of(context).push(MaterialPageRoute(builder: (_)=> stardium()));
+          },
+
+          image: image,
+        ),
+        Text(
+          sportName,
+          style: const TextStyle(fontSize: 10, fontWeight: FontWeight.bold),
+        ),
+      ],
+    );
+  }
+  Column _buildIcon4(BuildContext context, String image, String sportName) {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        MyButton(
+          funButton: () {
+            Navigator.of(context).push(MaterialPageRoute(builder: (_)=> PlayerMovie()));
+          },
+          image: image,
+        ),
+        Text(
+          sportName,
+          style: const TextStyle(fontSize: 10, fontWeight: FontWeight.bold),
+        ),
+      ],
+    );
   }
 }
